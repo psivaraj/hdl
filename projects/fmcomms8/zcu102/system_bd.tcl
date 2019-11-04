e5109f473f4cbb186d42506cda1aa299be737314
Adrian Costina <adrian.costina@analog.com>
2019-11-04 15:57:11 +0000

    fmcomms8: ZCU102: Added DAC FIFO

--- projects/fmcomms8/zcu102/system_bd.tcl
+++ projects/fmcomms8/zcu102/system_bd.tcl
@@ -2,6 +2,9 @@
 source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
 
+## FIFO depth is 8Mb - 500k samples
+set dac_fifo_address_width 16
+
 #system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
